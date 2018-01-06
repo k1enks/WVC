a1e5d32af19482b4782fa5684073aa5cbf9d6d12
k1enks <anna.klenkar@gmail.com>
2018-01-06 15:35:01 -0500

    owl carousel unfixed

---
+++ js/main.js
@@ -0,0 +1,40 @@
+//Document Ready Function
+// Add this function to make sure whole page is loaded before any js/jquery runs
+$(document).ready( function(){
+
+
+
+  //Owl Carousel
+
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:0,
+      nav:false,
+      dots:false,
+      autoplay:true,
+      slideBy:1,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+
+
+  })
+
+
+
+
+
+
+
+
+
+  //End of Document Ready function; should always be at very bottom
+  });
